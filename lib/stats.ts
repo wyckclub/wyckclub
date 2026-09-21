7025714eb47a30fd4bde5504aef5d0236686dce0
wyckclub <davidweinberg1102@gmail.com>
2026-09-22 03:39:18 +0700

    fix

--- lib/stats.ts
+++ lib/stats.ts
@@ -66,7 +66,7 @@ const ROBINHOOD_CATEGORY_URLS = [
 
   // process.env.WYCK_ROBIN1_URL,
   // process.env.WYCK_ROBIN1A_URL,
-  // process.env.WYCK_ROBIN1B_URL,
+  process.env.WYCK_ROBIN1B_URL,
   process.env.WYCK_ROBIN2_URL,
   process.env.WYCK_ROBIN2A_URL,
   process.env.WYCK_ROBIN2B_URL,
@@ -82,7 +82,7 @@ const ROBINHOOD_CATEGORY_URLS = [
   process.env.WYCK_ROBIN_PONSFAMILY4_URL,
   process.env.WYCK_ROBIN_PONSFAMILY5_URL,
   process.env.WYCK_ROBIN_PONSFAMILY6_URL,
-  // process.env.WYCK_ROBIN_PONSFAMILY7_URL,
+  process.env.WYCK_ROBIN_PONSFAMILY7_URL,
   // process.env.WYCK_ROBIN_PONSFAMILY8_URL,
 
   process.env.WYCK_ROBIN_LONG1_URL,
